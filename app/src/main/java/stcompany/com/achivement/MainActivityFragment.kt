02ebf75615769d5ca15d07ea754a0bad6d82533f
yoshida0261 <yoshida261@gmail.com>
2016-04-29 07:21:17 +0900

    kotlinに変更

---
+++ app/src/main/java/stcompany/com/achivement/MainActivityFragment.kt
@@ -0,0 +1,18 @@
+package stcompany.com.achivement
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+}
