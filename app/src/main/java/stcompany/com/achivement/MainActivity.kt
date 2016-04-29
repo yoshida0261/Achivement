ed659264a470b1e8bfd88dbd34886320a57544ef
yoshida0261 <yoshida261@gmail.com>
2016-04-30 03:32:11 +0900

    listView追加

--- app/src/main/java/stcompany/com/achivement/MainActivity.kt
+++ app/src/main/java/stcompany/com/achivement/MainActivity.kt
@@ -1,5 +1,6 @@
 package stcompany.com.achivement
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var  activity : Activity? = null
 
     /*
       todo
@@ -23,7 +30,24 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab?.setOnClickListener(View.OnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        fab?.setOnClickListener(View.OnClickListener {
+            view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+
+        activity = this
+
+        var list = ArrayList<String>()
+        for(a in 1..5){
+            list.add(a.toString())
+        }
+
+        var listView = findViewById(R.id.ListView) as ListView
+        var adapter = ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
+        listView?.setOnItemClickListener({
+            parent, view, position, id->
+             parent.getItemAtPosition(position)
+             Toast.makeText(activity, position.toString(), Toast.LENGTH_SHORT).show()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
